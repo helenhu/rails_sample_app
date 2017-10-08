9d869d3a5ee100d1e8e6a722bdfe5d74243b933d
Helen <hu.heleny@gmail.com>
2017-10-08 01:26:36 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,11 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'div.stats'
+    assert_match @user.followers.count.to_s, response.body
+    assert_match @user.following.count.to_s, response.body
   end
 end
